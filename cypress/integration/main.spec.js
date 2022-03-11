eb06aa3210e80494789108e840805876a7c186d4
suleyman <suleymanyaryev@gmail.com>
2022-03-11 23:22:24 +0500

    feat: add cypress e2e tests

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -1,5 +1,108 @@
 describe("Main page", () => {
-    it("Should visit main", () => {
+    beforeEach(() => {
+        cy.cleanDatabase();
+    });
+
+    it("creates todo item", () => {
         cy.visit("/");
+        cy.get("[data-cy='form-input']").type("test 1");
+        cy.get("[data-cy='form-submit']").click();
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").contains(
+            "test 1"
+        );
+
+        cy.waitForNetworkIdle(500);
+
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").should(
+            "have.length",
+            1
+        );
+
+        cy.get("[data-cy='form-input']").type("test 2");
+        cy.get("[data-cy='form-submit']").click();
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").contains(
+            "test 2"
+        );
+
+        cy.waitForNetworkIdle(500);
+
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").should(
+            "have.length",
+            2
+        );
+
+        cy.get("[data-cy='form-input']").type("test 2");
+        cy.get("[data-cy='form-submit']").click();
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").contains(
+            "test 2"
+        );
+
+        cy.waitForNetworkIdle(500);
+
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").should(
+            "have.length",
+            2
+        );
+    });
+
+    it("creates todo and changes it's status", () => {
+        cy.visit("/");
+        cy.get("[data-cy='form-input']").type("test");
+        cy.get("[data-cy='form-submit']").click();
+
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").each(($el) => {
+            cy.wrap($el).find("button[data-cy='btn-done']").contains("Done");
+        });
+
+        cy.get(
+            "[data-cy='todo-wrapper'] [data-cy='todo-item'] button[data-cy='btn-done']"
+        ).click();
+
+        cy.waitForNetworkIdle(500);
+
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").each(($el) => {
+            cy.wrap($el)
+                .find("button[data-cy='btn-undone']")
+                .contains("Undone");
+        });
+
+        cy.get(
+            "[data-cy='todo-wrapper'] [data-cy='todo-item'] button[data-cy='btn-undone']"
+        ).click();
+
+        cy.waitForNetworkIdle(500);
+
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").each(($el) => {
+            cy.wrap($el).find("button[data-cy='btn-done']").contains("Done");
+        });
+    });
+
+    it("creates todo and removes them", () => {
+        cy.visit("/");
+        cy.get("[data-cy='form-input']").type("test 1");
+        cy.get("[data-cy='form-submit']").click();
+
+        cy.waitForNetworkIdle(500);
+
+        cy.get("[data-cy='form-input']").type("test 2");
+        cy.get("[data-cy='form-submit']").click();
+
+        cy.waitForNetworkIdle(500);
+
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").should(
+            "have.length",
+            2
+        );
+
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").each(($el) => {
+            cy.wrap($el).dblclick();
+        });
+
+        cy.waitForNetworkIdle(500);
+
+        cy.get("[data-cy='todo-wrapper'] [data-cy='todo-item']").should(
+            "have.length",
+            0
+        );
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,11 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("cleanDatabase", () => {
+    cy.exec("psql -f ./backend/db.sql", {
+        env: {
+            PGDATABASE: Cypress.env("PGDATABASE"),
+            PGHOST: Cypress.env("PGHOST"),
+            PGPORT: Cypress.env("PGPORT"),
+            PGUSER: Cypress.env("PGUSER"),
+            PGPASSWORD: Cypress.env("PGPASSWORD"),
+        },
+    });
+});
